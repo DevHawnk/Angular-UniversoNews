044b5d4dc79f592aa7e5eb8af0452b90c9f21b05
DevHawnk <wesley.trabalho42@outlook.com>
2023-09-13 23:44:41 -0300

    commit de 13.09.23

---
+++ src/app/components/segunda-section/segunda-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segunda-section',
+  templateUrl: './segunda-section.component.html',
+  styleUrls: ['./segunda-section.component.css']
+})
+export class SegundaSectionComponent {
+
+}
