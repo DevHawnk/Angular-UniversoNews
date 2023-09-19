bec46eff0f325d1772e73d474be5853c538d405c
wesley souza pereira <103498566+DevHawnk@users.noreply.github.com>
2023-09-19 11:22:19 -0300

    atuali

--- src/app/page/content/content.component.ts
+++ src/app/page/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { datapage } from "";
 
 @Component({
   selector: "app-content",
@@ -14,4 +16,11 @@ export class ContentComponent {
   @Input()
   contantDescription: string =
     "Kaido, após derrubar Luffy várias vezes, não imaginava que, naquela última vez, após a intervenção de um membro da CP9, ele mataria Luffy. Ele só não contava com o despertar do Luffy.  Luffy, após a pancada de Kaido, se encontrava caído no chão, até o momento em que começou a se escutar um som que a criatura mais velha do mundo não escutava há 800 anos: os tambores da liberdade.<br>  Sim, ali estava iniciando o despertar da Akuma no Mi de Luffy. <br>  Aquele som era conhecido como os tambores da liberdade. <br>  Ali estava Luffy, retornando dos mortos, com um sorriso e uma força incontrolável. <br>  Nem Kaido acreditava, mas se empolgava por saber que teria uma luta que não tinha há muito tempo.  Só não esperava que a fruta do Luffy, na verdade, não era uma fruta comum, mas sim a fruta de Joy Boy, a fruta mítica do Deus Nika.Uma luta sem precedentes.";
+  private id: string | null = "0";
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => (this.id = value.get("id")));
+  }
+  setValuesToComponent(id: string) {}
 }
