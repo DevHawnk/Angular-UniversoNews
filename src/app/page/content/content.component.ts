dd23f95179186a5c7aff7c99264e3d630dda6d30
wesley souza pereira <103498566+DevHawnk@users.noreply.github.com>
2023-09-19 10:51:25 -0300

    19.09.23 atualizado

--- src/app/page/content/content.component.ts
+++ src/app/page/content/content.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  selector: "app-content",
+  templateUrl: "./content.component.html",
+  styleUrls: ["./content.component.css"],
 })
 export class ContentComponent {
-
+  @Input()
+  contantphoto: string =
+    "https://media.tenor.com/39hlFuVYE8IAAAAd/gear-5-gear-5-luffy.gif";
+  @Input()
+  contantTitle: string = "Luffy maceta Kaido";
+  @Input()
+  contantDescription: string =
+    "Kaido, após derrubar Luffy várias vezes, não imaginava que, naquela última vez, após a intervenção de um membro da CP9, ele mataria Luffy. Ele só não contava com o despertar do Luffy.  Luffy, após a pancada de Kaido, se encontrava caído no chão, até o momento em que começou a se escutar um som que a criatura mais velha do mundo não escutava há 800 anos: os tambores da liberdade.<br>  Sim, ali estava iniciando o despertar da Akuma no Mi de Luffy. <br>  Aquele som era conhecido como os tambores da liberdade. <br>  Ali estava Luffy, retornando dos mortos, com um sorriso e uma força incontrolável. <br>  Nem Kaido acreditava, mas se empolgava por saber que teria uma luta que não tinha há muito tempo.  Só não esperava que a fruta do Luffy, na verdade, não era uma fruta comum, mas sim a fruta de Joy Boy, a fruta mítica do Deus Nika.Uma luta sem precedentes.";
 }
