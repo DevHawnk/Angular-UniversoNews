79fb8f9aca43dc35cb6468c0ea8c37e1b571dec9
wesley souza pereira <103498566+DevHawnk@users.noreply.github.com>
2023-09-19 13:49:19 -0300

    atualizado as 13:48 19/09/2023

---
+++ src/app/data/dataPage.ts
@@ -0,0 +1,14 @@
+export const dataPage = [
+  {
+    id: "1",
+    title: "",
+    description: "",
+    photoCover: "",
+  },
+  {
+    id: "2",
+    title: "",
+    description: "",
+    photoCover: "",
+  },
+];
